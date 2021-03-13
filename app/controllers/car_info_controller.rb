2475dc62c84408e96fbd6198085bf087fc29df6e
erikn775 <erikn775@gmail.com>
2021-03-13 14:24:06 -0700

    added create method to car and home controller

--- app/controllers/car_info_controller.rb
+++ app/controllers/car_info_controller.rb
@@ -1,2 +1,15 @@
 class CarInfoController < ApplicationController
+
+    def create
+        car_info = CarInfo.new(car_params)
+        if car_info.save
+            render json: car_info
+        end
+    end
+
+    private
+
+    def car_params
+        params.require(:car_info).permit(:name, :email, :ownACar, :mpg, :type, :chargeAtHome)
+    end
 end
--- app/controllers/home_info_controller.rb
+++ app/controllers/home_info_controller.rb
@@ -1,2 +1,15 @@
 class HomeInfoController < ApplicationController
+
+    def create
+        home_info = HomeInfo.new(home_params)
+        if home_info.save
+            render json: home_info
+        end
+    end
+
+    private
+
+    def home_params
+        params.require(:home_info).permit(:name, :email, :typeOfHome, :solars, :waterHeater, :appliances, :lowFlow, :windows, :heaters, :lightBulbs)
+    end
 end
