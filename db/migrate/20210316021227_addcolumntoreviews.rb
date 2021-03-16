377a3176981b6e0f6ad6ba86e6f69275c8977eda
erikn775 <erikn775@gmail.com>
2021-03-15 20:28:16 -0600

    added column

---
+++ db/migrate/20210316021227_addcolumntoreviews.rb
@@ -0,0 +1,5 @@
+class Addcolumntoreviews < ActiveRecord::Migration[6.1]
+  def change
+    add_column :reviews, :rating, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_182036) do
+ActiveRecord::Schema.define(version: 2021_03_16_021227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_182036) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
   end
 
   create_table "solars", force: :cascade do |t|
